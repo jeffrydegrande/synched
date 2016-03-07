ed032fdc5b8e525ba574c7f4fe84448087bfe3c8
Jeffry Degrande <jeffry@jeffrydegrande.com>
2016-03-06 20:51:07 -0800

    no need for timeout in test

--- test/synched/exec_test.exs
+++ test/synched/exec_test.exs
@@ -3,17 +3,6 @@ defmodule Synched.ExecTest do
 
   test "it caches the function" do
     assert Synched.exec( "foobar", fn -> "foo" end) == "foo"
-    assert Synched.exec( "foobar", fn -> "notfoo" end) == "foo"
-  end
-
-  test "it waits for the result of the first call" do
-    assert Synched.exec( "2nd test", fn ->
-      :timer.sleep(2000)
-      "foo"
-    end) == "foo"
-
-    assert Synched.exec( "2nd test", fn ->
-      flunk "this shouldn't be called"
-    end) == "foo"
+    assert Synched.exec( "foobar", fn -> flunk "Shouldn't be called"  end) == "foo"
   end
 end
