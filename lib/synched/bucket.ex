c687f89909b94c3eed9b8ebc12b9fac0aae714a9
Jeffry Degrande <jeffry@jeffrydegrande.com>
2016-03-06 21:12:23 -0800

    convert to a gen server so it can update itself

--- lib/synched/bucket.ex
+++ lib/synched/bucket.ex
@@ -1,23 +1,42 @@
 defmodule Synched.Bucket do
-  def start_link() do
-    Agent.start_link(fn -> :no_value end)
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok)
   end
 
-  @doc "Retrieves the state of the agent or updates it by calling a function" 
+  def init(:ok) do
+    {:ok, {:no_value, nil}}
+  end
+
   def get(bucket, func) do
-    Agent.get_and_update(bucket, fn state ->
-      case state do
-        :no_value ->
-          new_state = func.()
-          {new_state, new_state}
-        ^state ->
-          {state, state}
-      end
-    end)
+    GenServer.call(bucket, {:get, func})
+  end
+  
+  def put(bucket, func) do
+    GenServer.call(bucket, {:put, func})
   end
 
-  @doc "Overwrites the state of the agent"
-  def put(bucket, func) do
-    Agent.update(bucket, fn x -> func.() end)
+  def handle_call({:get, func}, _from, state) do
+    case state do
+      {:no_value, _} ->
+        new_value = func.()
+        # after a second we're going to update ourselves
+        Process.send_after(self(), :refresh, 1000) 
+        {:reply, new_value, {new_value, func}}
+      {new_value, _} ->
+        {:reply, new_value, {new_value, func}}
+    end
+  end
+
+  def handle_call({:put, func}, _from, state) do
+    new_value = func.()
+    {:reply, new_value, {new_value, func}}
+  end
+
+  def handle_info(:refresh, {value, func}) do
+    IO.puts "updating"
+    Process.send_after(self(), :refresh, 1000) 
+    {:noreply, {func.(), func}}
   end
 end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -5,6 +5,7 @@ defmodule Synched.PageController do
   def index(conn, params) do
     wait = Map.get(params, "wait", "0")
     result = Synched.exec("Hello World", fn ->
+      IO.puts "Running the code with " <> wait
       :timer.sleep(500 * String.to_integer(wait))
       "got it: " <> wait
     end)
